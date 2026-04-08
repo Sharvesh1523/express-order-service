e43b1594e3ee4b9acbb3702b6eb70d8102098221
Sharvesh <siriusindia.mobile@gmail.com>
2026-04-08 20:19:07 +0530

    Added Loki Logger

--- logger.js
+++ logger.js
@@ -1,11 +1,35 @@
 const pino = require("pino");
 
-const logger = pino({
-  level: "info",
-  timestamp: pino.stdTimeFunctions.isoTime,
-  base: {
-    service: "order-service",
-  }
+const transport = pino.transport({
+  target: "pino-loki",
+  options: {
+    host: "http://loki-gateway.loki.svc.cluster.local", 
+    
+    // The multi-tenancy header Loki is expecting
+    headers: {
+      "X-Scope-OrgID": "tenant1", 
+    },
+    
+    // Labels help you search in Grafana (service name is already handled by your base object)
+    labels: {
+      env: "development" 
+    },
+    
+    // Batching sends logs in chunks to avoid overwhelming the network
+    batching: true,
+    interval: 5, 
+  },
 });
+
+const logger = pino(
+  {
+    level: "info",
+    timestamp: pino.stdTimeFunctions.isoTime,
+    base: {
+      service: "order-service",
+    },
+  },
+  transport
+);
 
 module.exports = logger;
